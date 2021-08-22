c1befc35011f81f700e15b0fe50bd49527a693b6
warbl <alexliu401@gmail.com>
2021-08-22 15:57:07 -0400

    Update stringshift.py
    completed

--- stringshift.py
+++ stringshift.py
@@ -11,7 +11,6 @@ Created on Sun Aug 22 15:33:33 2021
 def main():
     userString = input("Enter a string: ")
     userList = list(userString)
-    print(userList)
     
     rotations = int(input("Enter an integer to shift the string by: "))
     queue = []
@@ -22,7 +21,8 @@ def main():
     
     while(queue):
         userList.insert(0, queue.pop(0))
-    
+
+    print("\nYour new string is: " + ''.join(userList))
 
 if __name__ == "__main__":
     main()
